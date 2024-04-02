659667fcf670e337363c87f15269201549ffbc84
im-adithya <imadithyavardhan@gmail.com>
2024-04-02 17:24:38 +0530

    chore: add index to subscriptions uuid column

--- migrations/202404021628_add_uuid_to_subscriptions.go
+++ migrations/202404021628_add_uuid_to_subscriptions.go
@@ -9,9 +9,15 @@ import (
 var _202404021628_add_uuid_to_subscriptions = &gormigrate.Migration{
 	ID: "202404021628_add_uuid_to_subscriptions",
 	Migrate: func(tx *gorm.DB) error {
-		return tx.Exec("ALTER TABLE subscriptions ADD COLUMN uuid UUID DEFAULT gen_random_uuid()").Error
+		if err := tx.Exec("ALTER TABLE subscriptions ADD COLUMN uuid UUID DEFAULT gen_random_uuid()").Error; err != nil {
+			return err
+		}
+		return tx.Exec("CREATE INDEX IF NOT EXISTS subscriptions_uuid ON subscriptions (uuid)").Error
 	},
 	Rollback: func(tx *gorm.DB) error {
+		if err := tx.Exec("DROP INDEX IF EXISTS subscriptions_uuid").Error; err != nil {
+			return err
+		}
 		return tx.Exec("ALTER TABLE subscriptions DROP COLUMN IF EXISTS uuid").Error
 	},
 }
